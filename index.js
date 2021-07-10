a416c1496982330ef92d4a9ad4731eb561e6a5be
Reggie12 <regina.schmidt1212@gmail.com>
2021-07-10 19:39:15 +0200

    Minus src

---
+++ index.js
@@ -0,0 +1,140 @@
+body {
+  font-family: sans-serif;
+  background: #ebdbea;
+}
+
+.toDo {
+  margin: 200px 0px 0px 50px;
+  border: width 1px;
+  border: solid;
+  background: #edf3f1;
+  border-radius: 2% 6% 5% 4% / 1% 1% 2% 4%;
+  opacity: 70%;
+  padding: 0 20px 0 10px;
+  box-shadow: 8px 14px 10px grey;
+}
+.toDo::before {
+  content: " ";
+  margin: -200px -50px 2px -10px;
+  border: width 1px;
+  border: solid;
+
+  padding: 0% 20px 480px 296px;
+  padding-right: 8%;
+  transform: translate3d(-50%, -50%, 0) scale(1.015) rotate(0.5deg);
+  border-radius: 1% 1% 2% 4% / 2% 6% 5% 4%;
+  opacity: 70%;
+}
+.titleToDo {
+  font-family: "Indie Flower", cursive;
+  font-size: 26px;
+  font-weight: bolder;
+  border: width 1px;
+  border: solid;
+  border-radius: 1% 1% 2% 4% / 2% 6% 5% 4%;
+  background: #edf3f1;
+  margin-top: 5px;
+  display: block;
+  text-align: center;
+  padding: 5px;
+  margin: 30px 15px 15px 15px;
+  opacity: 75%;
+}
+.ideas {
+  padding-top: 20px;
+  padding-left: 55px;
+  padding-right: -10px;
+  font-family: "Indie Flower", cursive;
+  font-size: 24px;
+  opacity: 80%;
+  box-shadow: 60%;
+}
+.idea1 {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.idea2 {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.idea3 {
+  padding-top: 20px;
+  padding-bottom: 60px;
+}
+.weather {
+  margin: -460px 0px 300px 535px;
+  padding-left: 0;
+  padding-right: 0;
+  border: width 1px;
+  border: solid;
+  background: #edf3f1;
+  opacity: 70%;
+}
+.weatherSearch {
+  margin: 3% 1% 3% 5%;
+  opacity: 70%;
+  padding-left: 5%;
+  font-size: 23px;
+}
+.btn-info {
+  margin-left: -7%;
+  margin-top: 8%;
+  margin-bottom: 1%;
+  margin-right: 0%;
+  padding-left: 30%;
+  padding-right: 30%;
+  border: solid;
+  border-color: #d1dcf0;
+  opacity: 70%;
+  background-color: #d1dcf0;
+}
+.underline {
+  color: rgb(8, 8, 8);
+  font-weight: bolder;
+  margin: 3.2% 3% 4% 4%;
+  position: absolute;
+  opacity: 70%;
+}
+.cityToday {
+  padding: 3% 1% 0 7%;
+  margin: 1% 4% 1% 2%;
+}
+.cityDisplay {
+  text-decoration: underline dotted;
+  padding: 0% 5% 0% 0%;
+  font-size: 30px;
+  text-align: center;
+}
+.forecast {
+  margin: 4% 5% 4% 2%;
+  padding: 0;
+  text-align: center;
+  border: none;
+}
+.list-group-item {
+  background: #edf3f1;
+}
+.additionalInfo {
+  padding-top: 4%;
+  line-height: 0.9;
+  opacity: 70%;
+}
+.todayTemp {
+  padding: 0% 5% 0% 0%;
+  font-size: 27px;
+  text-align: center;
+  font-weight: bolder;
+  opacity: 80%;
+}
+.minMax {
+  border: solid;
+  border-width: 9%;
+  opacity: 60%;
+  border-radius: 1% 1% 2% 4% / 2% 6% 5% 4%;
+  text-align: center;
+  padding: 0.3%;
+  margin: 2% 23% 15% 15%;
+  background: rgb(207, 203, 203);
+  border-width: lighter;
+  font-weight: bold;
+}
